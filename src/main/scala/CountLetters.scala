82bf64185b3bc2ccf6f1068e7f92480976544ad5
ek <evija.kvante@inbox.lv>
2022-03-24 20:48:17 +0200

    My try!

---
+++ src/main/scala/CountLetters.scala
@@ -0,0 +1,33 @@
+import scala.Console.println
+import scala.io.StdIn.readLine
+
+object CountLetters extends App {
+
+  //TODO ask user for some sentence or word
+  //Count the number of characters in this string
+  //Store them in a mutable Map of type Char, Int
+  //print the results
+  //you can count whitespace as well
+
+  //you could store results in some sort of Sequence and then convert to Map later, but that would just complicate things
+
+  val sentence = readLine("Please enter some sentence of word:\n")
+  println(sentence)
+
+  val count = sentence.length
+  println(count)
+
+  val word = sentence.split(" ")
+  println(s"The words from sentence:\n${word.mkString(",")}")
+
+  val numberOfCharacters = word.map(_.length)
+  println(s"Word lengths:\n${numberOfCharacters.mkString(",")}")
+
+  val mutMap = scala.collection.mutable.Map(word-> numberOfCharacters)
+  println(mutMap)
+
+  val mySeq = Seq((word,numberOfCharacters))
+  val myMap = mySeq.toMap
+  println(myMap)
+
+}
